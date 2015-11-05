e22dd92e61573ae10a2415bdc0a71a02ec94bd3c
Pablo N Dalmasso <pablodalmasso@gmail.com>
2015-11-05 19:54:24 -0300

    Se agregan cambios

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,17 @@ var helloWorldApp = angular.module('helloWorldApp', [
 	'ngRoute',
 	'helloWorldControllers'
 ]);
+
+/* chapter1/app.js excerpt */
+helloWorldApp.config(['$routeProvider', '$locationProvider',
+function($routeProvider, $locationProvider) {
+	$routeProvider.
+	when('/', {
+		templateUrl: 'partials/main.html',
+		controller: 'MainCtrl'
+	}).
+	when('/show',{
+		templateUrl: 'partials/show.html',
+		controller: 'ShowCtrl'
+	})
+}])
